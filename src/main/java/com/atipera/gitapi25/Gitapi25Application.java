61d942ee25d5d447b1248cf0d401ceb016798a30
Karlz-Bandz <karol2646@gmail.com>
2026-05-27 12:21:33 +0200

    Init

---
+++ src/main/java/com/atipera/gitapi25/Gitapi25Application.java
@@ -0,0 +1,13 @@
+package com.atipera.gitapi25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitapi25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitapi25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/atipera/gitapi25/Gitapi25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atipera.gitapi25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitapi25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
